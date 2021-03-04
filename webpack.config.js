f21c3f4a1ef77c3ba87d979c0a82373fb3fffbbe
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-03-04 22:47:56 +0900

    Use webpack default

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,14 @@
-const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  entry: "./index.js",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./index.html",
+      template: "./src/index.html",
     }),
   ],
 };
