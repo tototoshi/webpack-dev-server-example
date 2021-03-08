99f6d96535c7c5568b1752dd4ce153eb4e15e3c8
Toshiyuki Takahashi <t.toshi.0412@gmail.com>
2021-03-08 22:11:14 +0900

    Add target and comments

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  // https://github.com/webpack/webpack-dev-server/issues/2758
+  // https://github.com/pmmmwh/react-refresh-webpack-plugin/blob/main/docs/TROUBLESHOOTING.md#webpack-5-compatibility-issues-with-webpack-dev-server3
+  // In webpack-dev-server@3, there is a bug causing it to mis-judge the runtime environment when the Webpack 5 browserslist target is used.
+  target: "web",
   mode: "development",
   devtool: "inline-source-map",
   devServer: {
